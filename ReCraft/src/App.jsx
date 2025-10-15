b46f67aeb9be888f2388d010baeb84410b6117a4
Ana <ap10332n@pace.edu>
2025-10-15 15:58:34 -0400

    Text changes

--- ReCraft/src/App.jsx
+++ ReCraft/src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>ReCraft</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Team 2
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Ana Paredes, Diya Brown, Rui Wang, Jessica Williamson
       </p>
     </>
   )
