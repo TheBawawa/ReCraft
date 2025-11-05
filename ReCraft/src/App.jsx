e238b75ddbaccd6f46c43c08b4ede61a1788d7a6
db71722n <db71722n@pace.edu>
2025-11-05 11:49:28 -0500

    Fixing App.jsx file to test webpage

--- ReCraft/src/App.jsx
+++ ReCraft/src/App.jsx
@@ -2,19 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";      // top navigation bar
-import Home from "./pages/Home";               // homepage
-import About from "./pages/About";             // about page
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
     <Router>
-      {/* Global Navbar (always visible, even while scrolling) */}
       <Navbar />
-
-      {/* Main Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
