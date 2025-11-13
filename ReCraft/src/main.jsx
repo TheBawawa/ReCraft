84d3091202a85201114b2665a740fe1e4ca7f4a7
Ana <ap10332n@pace.edu>
2025-11-13 17:11:55 -0500

    wuuuu

--- ReCraft/src/main.jsx
+++ ReCraft/src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
-//import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
