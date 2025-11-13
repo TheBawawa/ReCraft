8711ed1c26d80882ce6f4cee3710e5ea459c5805
Ana <ap10332n@pace.edu>
2025-11-13 17:02:26 -0500

    trynna repair my branch :<

---
+++ ReCraft/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import PostTemplate from "../components/PostTemplate";
+import "../App.css";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="center-content">
+      <h1 style={{ color: "var(--blue-gray)" }}>ReCraft</h1>
+      <p style={{ color: "var(--coral)" }}>Team 2</p>
+      <p style={{ color: "var(--blue-gray)" }}>
+        Ana Paredes — Diya Brown — Rui Wang — Jessica Williamson
+      </p>
+
+      <button className="circle-btn" onClick={() => navigate("/login")}>
+        TEMPORAL ANA
+      </button>
+
+      <PostTemplate />
+    </div>
+  );
+}
+//not touch
+
+export default Home;
